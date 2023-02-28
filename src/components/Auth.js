e8eeb07e4d8b310dbf141cc7e30779ac5c70e6cc
jdbm69 <104793276+jdbm69@users.noreply.github.com>
2023-02-28 11:39:23 -0400

    Update Auth.js

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -45,10 +45,10 @@ const Auth = () => {
             <div className='auth-box'>
                 <form>
                     <div className='auth-title'>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="rgb(92, 168, 168)" class="bi bi-list-check" viewBox="0 0 16 16">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="rgb(92, 168, 168)" class="bi bi-list-check" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M5 11.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3.854 2.146a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708L2 3.293l1.146-1.147a.5.5 0 0 1 .708 0zm0 4a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708L2 7.293l1.146-1.147a.5.5 0 0 1 .708 0zm0 4a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 0 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0z"/>
                         </svg>
-                        <h1>TO DO APPP</h1>
+                        <h1>TO DO APP</h1>
                     </div>
                     <h2>{isLogIn ? 'Please Log In' : 'Please Sign Up'}</h2>
                     <input type='email' placeholder='Email' onChange={(e) => setEmail(e.target.value)} />
